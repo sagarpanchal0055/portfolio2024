c8364edf5f2f321408fafe81af7576eb5fa306f7
Sagar Panchal <sagar.panchal0055@gmail.com>
2024-08-26 23:40:03 +0530

    Changes

--- src/lib/content/hero.ts
+++ src/lib/content/hero.ts
@@ -6,7 +6,7 @@ export const heroSection: HeroSectionType = {
   title: 'Sagar Panchal.',
   tagline: 'I create feature-rich, scalable interfaces for the web.',
   description:
-    "I'm a passionate Front-End web developer with hands-on 4 years of experience in building web applications using React.js & Next.js with Javascript (ES5/ES6), TailwindCSS, and Redux.",
+    "I'm a passionate Front-End web developer with hands-on 3+ years of experience in building web applications using React.js & Next.js with Javascript (ES5/ES6), TailwindCSS, and Redux.",
   specialText: 'Currently available for new opportunity ',
   cta: {
     title: 'see my resume',
